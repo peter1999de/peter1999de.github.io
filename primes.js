a264c93a9560ec95792904fa2b825498fc451f44
Peter Fichtelmann <peter1999de@gmail.com>
2016-02-06 17:58:57 +0100

    create primes page

---
+++ primes.js
@@ -0,0 +1,36 @@
+console.log("loaded");
+
+(function() {
+var input = document.getElementById("in");
+console.log(input);
+input.value=47;
+	var output = document.getElementById("out");
+	var go = document.getElementById("go");
+
+	function findPrimes(max) {
+		var primes = [2];
+		var n = 3;
+		while(n<max) {
+			var isPrime = true;
+			primes.forEach(function(prime) {
+				if (n % prime === 0) {
+					isPrime = false;
+				}
+			});
+			if (isPrime) {
+				primes.push(n);
+			}
+			n++;
+		}
+		return primes;
+	}
+	go.onclick= function() {
+		output.innerHTML = '';
+		var primes = findPrimes(input.value);
+		primes.forEach(function (prime) {
+			var entry = document.createElement("span");
+			entry.textContent = prime;
+			output.appendChild(entry);
+		});
+	}
+})();
